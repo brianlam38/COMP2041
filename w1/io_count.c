dca8c5e6298a13b969e35a6235f88fc9e52bb3bb
brianlam38 <brian.lam38@gmail.com>
2017-08-02 15:39:45 +1000

    updating io_count program

--- w1/io_count.c
+++ w1/io_count.c
@@ -36,5 +36,22 @@ void process_stream(FILE *in) {
 	}
 }
 
-// count lines, words, chars in stream
-void count
+// Count lines, words, chars in stream
+// This program mimics the Word Count tool in unix
+void count_file(FILE *in) {
+	int n_lines = 0, n_words = 0, n_chars = 0;
+	int in_word = 0, c;
+	while ((c = fget(in)) != EOF) {
+		n_chars++;
+		if (c == '\n') n_lines++;
+		if (isspace(c)) in_word = 0;
+		else if (!in_word) {
+			in_word = 1;
+			n_words++;
+		}
+	}
+	printf("%6d %6d %6d", n_lines, n_words, n_chars);
+}
+
+
+
