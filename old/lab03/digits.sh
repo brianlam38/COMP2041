fb6aae61f5875c538d2b902c8cd4c08f67cda93b
brianlam38 <brian.lam38@gmail.com>
2017-08-14 00:09:28 +1000

    updating all files

--- old/lab03/digits.sh
+++ old/lab03/digits.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#replaces all numbers less than 5 with <
-#replaces all numbers greater than 5 with >
+#replace all numbers less than 5 with <
+#replace all numbers greater than 5 with >
 
 tr '0123456789' '<<<<<5>>>>'
