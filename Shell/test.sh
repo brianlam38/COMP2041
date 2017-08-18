d6193d3834e222acfc30efa39fd27d4d5cfd3d23
brianlam38 <brian.lam38@gmail.com>
2017-08-18 16:36:32 +1000

    updating shell scripts

--- Shell/test.sh
+++ Shell/test.sh
@@ -1,19 +1,4 @@
 #!/bin/sh
 
-x=1
-y=fred
-echo $x$y
-
-echo $xy # the aim is to display "1y"
-
-echo "$x"y
-
-echo ${x}y
-
-echo ${j-10} # give value of j or 10 if no value
-
-echo ${j=33} # set j to 33 if no value (and give £j)
-
-echo ${x:?No Value} # display "No Value" if £x not set
-
-echo ${xx:?No Value} # display "No Value" if £xx not set
+msg="hello there"
+test $msg = Hello
