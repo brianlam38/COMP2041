0d1ac386a7413f392be06bac390dd467d4f91096
brianlam38 <brian.lam38@gmail.com>
2017-08-26 12:12:07 +1000

    adding digits perl program

--- Shell/variables.sh
+++ Shell/variables.sh
@@ -1,22 +1,14 @@
 #!/bin/sh
 
 # Shell doesn't care about type in variables.
-
-
 x=1
 y=fred
 echo $x$y
-
 echo $xy # the aim is to display "1y"
-
 echo "$x"y
-
 echo ${x}y
-
 echo ${j-10} # give value of j or 10 if no value
-
 echo ${j=33} # set j to 33 if no value (and give £j)
-
 echo ${x:?No Value} # display "No Value" if £x not set
-
 echo ${xx:?No Value} # display "No Value" if £xx not set
+
