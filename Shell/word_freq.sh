cf13832b6e437168a82af57c62a13471179c6fb5
brianlam38 <brian.lam38@gmail.com>
2017-08-29 16:46:58 +1000

    updating word frequency shell script

--- Shell/word_freq.sh
+++ Shell/word_freq.sh
@@ -3,7 +3,13 @@
 # Count the number of tmess each different word occurs in the files given as args
 # USAGE: ./word_freq.sh file.txt
 
-sed 's/ /\n/g' "$@" |			# "For all occurences of a space, replace with a newline"
-									# converts to one word per line (sed = stream editor)
-									# s/RegExp/Replace/g
-tr A-Z a-z |					# map uppercase to lower case (tr = transliterate)
+sed 's/ /\n/g' "$@" |		# "For all occurences of a space, replace with a newline"
+								# converts to one word per line (sed = stream editor)
+tr A-Z a-z |				# "Substitute upper letters to lower letters + print to output"
+								# map uppercase to lower case (tr = transliterate)
+sed "s/[^a-z']//g" |		# Remove all chars except a-z and '
+egrep -v '^$' |				# Remove empty lines
+sort |						# Sort words in alphabetical order
+uniq -c |					# Precede each output line with the number of times the line has occurred
+sort -n 					# Sort words in numerical order
+								# words will be ordered by the occurrence number preceding each line
