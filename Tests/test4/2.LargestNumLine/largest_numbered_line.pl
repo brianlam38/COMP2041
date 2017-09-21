47dc16cb9d58e1de97de84ff6db7cece163f8288
brianlam38 <brian.lam38@gmail.com>
2017-09-21 17:33:38 +1000

    updating test4 perl script

--- Tests/test4/2.LargestNumLine/largest_numbered_line.pl
+++ Tests/test4/2.LargestNumLine/largest_numbered_line.pl
@@ -15,31 +15,50 @@ while ($line = <STDIN>) {
 		# remove all but numbers, add to line's array
 		chomp($item);
 		if ($item =~ /\d/) {
-			$item =~ s/[a-zA-Z]?[\$]?[-]?//g;	# remove alphabet, -, $
-			#print $item."\n";
+			$item =~ s/[a-zA-Z]?[\$]?//g;	# remove alphabet, -, $
+			#print "item to push = ".$item."\n";
 			push(@final, $item);		# add num values for each line
 		}
 	}
 	# only map lines with numbers
 	$count = @final;
+	#print "count = ".$count;
 	#print "NUMBERS IN LINE = ".$count."\n";
 	if ($count > 0) {
+		#print max(@final)."\n";
 		$map{$lineNum} = max(@final);
-		#print $map{$line}."\n";
+		#print $map{$lineNum}."\n";
 	}
 	$lineNum++;
 }
 
-$largest = 0;
+$largest = -1;
+$k = 0;
 @largest_lines;
 foreach $key (keys %map) {
-	#print $key;
-	#print $map{$key}."\n";
-	if ($map{$key} >= $largest) {		# if larger or equal
-		push (@largest_lines, $key);	# add line with largest num into array
+	# post-processing: removing duplicate dash e.g. --90 -> -90
+	if ($map{$key} =~ /--/) {
+		$map{$key} =~ s/--/-/g;
+	}
+	#print "val = ".$map{$key}."\n";
+	#print "largest = ".$largest."\n";
+	# add diff line with same value
+	if ($map{$key} == $largest) {
+		push (@largest_lines, $key);
+		next;
+	}
+	# add largest val line
+	if ($map{$key} > $largest) {		# if larger or equal
 		$largest = $map{$key};			# update largest num
+		$k = $key;
 	}
 }
+
+if ($largest == -1) {
+	exit;
+}
+
+push (@largest_lines, $k);	# add line with largest num into array
 
 # numeric ascending sort line indices
 @largest_lines = sort {$a <=> $b} @largest_lines;
