eee2a4253b3e4186e19eee2e2338e6bf570bf24f
brianlam38 <brian.lam38@gmail.com>
2017-11-04 23:22:51 +1100

    updating equi word py script

--- PRAC/W10/equi_filter.py
+++ PRAC/W10/equi_filter.py
@@ -4,28 +4,28 @@ import sys
 import re
 
 for line in sys.stdin:
-	line = line.replace("\n", "")
-	array = line.split(" ")
-	final_str = ""
-	# each word in line
-	for i in array:
-		#print("WORD = {}".format(i))
-		chars = list(i)
-		# count each char in word
-		equi_word = {}
-		for c in chars:
-			c = c.lower()
-			#print("CHAR = {}".format(c))
-			if c in equi_word:
-				equi_word[c] += 1
-			else:
-				equi_word[c] = 0
-		# check if equi_word
-		equi = 0
-		for key, val in equi_word.items():
-			#print("VAL = {}".format(val))
-			if val != equi_word.items()[0][1]:
-				equi = 1
-		if equi is 0:
-			final_str += i + " "
-	print(final_str)	
+        line = line.replace("\n", "")
+        array = line.split(" ")
+        final_str = ""
+        # each word in line
+        for i in array:
+                chars = list(i)
+                # count each char occurrnece
+                equi_word = {}
+                for c in chars:
+                        c = c.lower()
+                        if c in equi_word:
+                                equi_word[c] += 1
+                        else:
+                                equi_word[c] = 0
+                # check if equi_word
+                equi = 0
+                for key, val in equi_word.items():
+                        if val != equi_word.items()[0][1]:
+                                equi = 1
+                # word = equi_world, add to final str
+                if equi is 0:
+                        final_str += i + " "
+        # print equi_word str
+        print(final_str)
+
